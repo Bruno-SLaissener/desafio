0c37e7e25fe99bab24df5068bf0490cdaf1e7509
blaissener <blaissener@proton.me>
2023-10-23 21:41:11 -0300

    gst split working. Trying to build the .engine file

--- app_code/main.cpp
+++ app_code/main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 
 #include <gst/gst.h>
-#include "yolov5_detector.hpp"
+//#include "yolov5_detector.hpp"
+#include "yolov5_builder.hpp"
 
+// namespace bClass{
+//     #include "yolov5_builder.hpp"
+// }
+
+void builder(void){
+    yolov5::Precision precision = yolov5::PRECISION_FP32;
+    yolov5::Builder builder;
+    yolov5::Result r = builder.init();
+    r = builder.buildEngine("./yolov5s.onnx", "./yolov5s.engine", precision);
+}
 
 
 int main(int arg, char *argv[]) {
+    
+    builder();
 
     //Testing the yolo lib
-    yolov5::Detector detector;
-    yolov5::Result r = detector.init();
-
+    //yolov5::Detector detector;
+    //yolov5::Result r = detector.init();
+    
     GstElement *pipeline = nullptr;
     GstBus *bus = nullptr;
     GstMessage *msg = nullptr;
@@ -19,13 +32,19 @@ int main(int arg, char *argv[]) {
     gst_init(&arg, &argv);
 
     // building pipeline
-    pipeline = gst_parse_launch(
-            "v4l2src device=/dev/video0 ! xvimagesink",
+    pipeline = gst_parse_launch("v4l2src device=/dev/video2 ! tee name=t \ 
+                                t. ! video/x-raw,format=YUY2,width=640,height=480,framerate=30/1 ! queue !  identity ! shmsink wait-for-connection=0 socket-path=/app_code/tmp/tmpsock  shm-size=20000000 sync=true \
+                                t. ! xvimagesink ",
             nullptr);
-            //"playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm",
+
+    //cv::VideoCapture capture;
+    //capture.open("shmsrc socket-path=/app_code/tmp/tmpsock  ! video/x-raw, format=YUY2, width=640, height=480, framerate=30/1 ! decodebin ! videoconvert ! appsink");
+
+    //std::cout << capture.isOpened() << std::endl;
 
     // start playing
     gst_element_set_state(pipeline, GST_STATE_PLAYING);
+
 
     //wait until error or EOS ( End Of Stream )
     bus = gst_element_get_bus(pipeline);
